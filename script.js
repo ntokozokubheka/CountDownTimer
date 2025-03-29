dd754f3945513c6a8d270e0277444a5b02fea50f
ntokozo <kubhekantozo@hotmail.com>
2025-03-29 08:23:41 -0500

    updates

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secsEl = document.getElementById('seconds');
 
-const daysEl =document.getElementById('days');
-const hoursEL=document.getElementById('hours');
-const secsEL = document.getElementById('seconds');
-const minsEL = document.getElementById('mins');
+const newYears = "1 Jan 2026";
 
-const newYears="1 Jan 2023";
+function countDown() {
+  const currentDate = new Date();
+  const newYearsDate = new Date(newYears);
+  const seconds = (newYearsDate - currentDate) / 1000;
 
+  if (seconds < 0) {
+    daysEl.innerHTML = '00';
+    hoursEl.innerHTML = '00';
+    minsEl.innerHTML = '00';
+    secsEl.innerHTML = '00';
+    return;
+  }
 
-function countDown()
-{
+  const days = Math.floor(seconds / 3600 / 24);
+  const hours = Math.floor(seconds / 3600) % 24;
+  const minutes = Math.floor(seconds / 60) % 60;
+  const secs = Math.floor(seconds) % 60;
 
-const currentDate = new Date(); // Read the dATE
-const newYearsDate = new Date (newYears);
-const seconds =(newYearsDate-currentDate)/1000; 
-const days=Math.floor(seconds / 3600/24);
-const hours=Math.floor(seconds / 3600)%24;
-const minutes =Math.floor(seconds /60)%60;
-const secs =Math.floor(seconds) % 60;
-daysEl.innerHTML =days;
-hoursEL.innerHTML=formatTime(hours);
-minsEL.innerHTML=formatTime(minutes);
-secsEL.innerHTML=formatTime(secs);
-
-
-
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(minutes);
+  secsEl.innerHTML = formatTime(secs);
 }
 
-
-function formatTime(time)
-{
-
-return time <10 ?`0${time}` :time ;
-
-
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
 }
 
 countDown();
-setInterval(countDown,1000);
+setInterval(countDown, 1000);
